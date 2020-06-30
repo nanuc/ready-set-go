be8324889585a30a587c9bb8b253b9aac8ae22dd
Sebastian Schöps <ssc@schoeps-consult.de>
2020-07-01 01:05:17 +0200

    Color correction

--- resources/views/components/tabs/tab-item.blade.php
+++ resources/views/components/tabs/tab-item.blade.php
@@ -1,6 +1,6 @@
 @push('tab-nav' . $tabsId)
     <button
-        :class="{ 'border-blue-700 text-blue-700 focus:text-blue-800 focus:border-blue-700': tab === '{{ $id }}' }"
+        :class="{ 'border-indigo-700 text-indigo-700 focus:text-indigo-800 focus:border-indigo-700': tab === '{{ $id }}' }"
         class="tab-link mr-8 whitespace-no-wrap py-2 px-1 border-b-2 border-transparent font-medium text-sm leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
         @click="tab = '{{  $id }}'">
         {{ $caption }}
